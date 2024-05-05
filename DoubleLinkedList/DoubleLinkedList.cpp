a846b4c6e33913ee70e549e50eeb89e79e690254
rcslbaa <ifasmarani3125@gmail.com>
2024-05-05 09:34:59 +0700

    commit 5

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -28,4 +28,10 @@ void addNode()
             cout << "\033[31mDuplicate roll number not allowed\033[0m" << endl;
             return;
         }
-        
+        // if the list is emty, make the new node the START
+        newNode->next = START; // step 3: make the new node point to the first node
+        if (START != NULL)
+        {
+            START->prev = newNode; // step 4: make the first node to the new node
+        }
+       
