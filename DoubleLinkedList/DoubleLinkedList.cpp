e244e5a16a6f8d3517ba1ca1c07de18bae728269
rcslbaa <ifasmarani3125@gmail.com>
2024-05-05 09:42:22 +0700

    commit 16

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -102,3 +102,9 @@ void deleteNode()
         previous = current;
         current = current->next;
     }
+
+    if (current == NULL)
+    {
+        cout << "\033[31mThe record with roll number " << rollNo << " not found\033[0m" << endl;
+        return;
+    }
