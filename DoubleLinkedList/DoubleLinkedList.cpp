0b0cb053875b6e6b5dbecf2d60062c25e0c4e77d
rcslbaa <ifasmarani3125@gmail.com>
2024-05-05 09:43:11 +0700

    commit 18

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -117,3 +117,12 @@ void deleteNode()
             START->prev = NULL;
         }
     }
+
+    else
+    {
+        previous->next = current->next;
+        if (current->next != NULL)
+        {
+            current->next->prev = previous;
+        }
+    }
