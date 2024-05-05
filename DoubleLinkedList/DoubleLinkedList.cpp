27623fd7169752a93749e0a4d756b43044456bbc
rcslbaa <ifasmarani3125@gmail.com>
2024-05-05 09:34:19 +0700

    commit 4

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -22,3 +22,10 @@ void addNode()
     // insert the new node in the list
     if (START == NULL || newNode->noMhs <= START->noMhs)
     {// step 2: insert the new node at the beginning
+
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll number not allowed\033[0m" << endl;
+            return;
+        }
+        
